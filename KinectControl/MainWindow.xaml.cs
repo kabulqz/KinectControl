bc294c991dfb1fad4cace37c495fb122efad8924
kabulqz <123zelek@gmail.com>
2025-06-27 19:28:50 +0200

    Add custom hand and drag cursors with mouse hook
    Introduces custom hand and drag cursor files and updates MainWindow.xaml.cs to set the system cursor based on mouse button events using a low-level mouse hook. The system cursor changes to a drag cursor on mouse down and to a hand cursor on mouse up, restoring defaults on window close.

--- KinectControl/MainWindow.xaml.cs
+++ KinectControl/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Channels;
@@ -16,33 +17,82 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using Application = System.Windows.Application;
 
 namespace KinectControl
 {
     public partial class MainWindow : Window
     {
+        private static void SetSystemCursorTo(string filePath)
+        {
+            var cursorPath = System.IO.Path.GetFullPath(filePath);
+            var hCursor = LoadCursorFromFile(cursorPath);
+            if (hCursor == IntPtr.Zero)
+            {
+                Console.WriteLine($@"LoadCursorFromFile failed. Error: {Marshal.GetLastWin32Error()}");
+                return;
+            }
+
+            if (!SetSystemCursor(hCursor, OCR_NORMAL))
+            {
+                Console.WriteLine($@"SetSystemCursor failed. Error: {Marshal.GetLastWin32Error()}");
+            }
+        }
+        private static void RestoreDefaultCursors()
+        {
+            SystemParametersInfo(SPI_SETCURSORS, 0, IntPtr.Zero, 0);
+        }
+        private IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int wmCode = wParam.ToInt32();
+
+                if (wmCode == WM_LBUTTONDOWN)
+                {
+                    // Zmień kursor na DragCur
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        SetSystemCursorTo(DragCur);
+                    });
+                }
+                else if (wmCode == WM_LBUTTONUP)
+                {
+                    // Zmień kursor na HandCur
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        SetSystemCursorTo(HandCur);
+                    });
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
         private const int GWL_EXSTYLE = -20;
         private const int WS_EX_TRANSPARENT = 0x00000020;
         private const int WS_EX_LAYERED = 0x00080000;
-        [DllImport("kernel32.dll")]
-        private static extern bool AllocConsole();
-        [DllImport("user32.dll")]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        private const int WH_MOUSE_LL = 14;
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_LBUTTONUP = 0x0202;
 
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool SystemParametersInfo(int uAction, uint uParam, IntPtr lpvParam, int fuWinIni);
-        [DllImport("user32.dll")]
-        private static extern bool SetSystemCursor(IntPtr hCursor, uint id);
-        [DllImport("user32.dll")]
-        private static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
-        [DllImport("user32.dll")]
-        private static extern bool DestroyCursor(IntPtr hCursor);
+        private IntPtr _hookID = IntPtr.Zero;
+        private LowLevelMouseProc _proc;
         private const uint OCR_NORMAL = 32512; // Standard arrow cursor
-        private const uint OCR_HAND = 32649; // Hand cursor
         private const int SPI_SETCURSORS = 0x0057;
+
+        private const string HandCur = @"../../Src/hand.cur";
+        private const string DragCur = @"../../Src/drag.cur";
         private Program program;
+
 
         public MainWindow()
         {
@@ -95,29 +145,50 @@ namespace KinectControl
                 var hwnd = new WindowInteropHelper(this).Handle;
                 var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
                 SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED);
-                SetSystemCursorToHand();
             };
-
+#endif
+            // Set up the mouse hook
+            _proc = HookCallback;
+            _hookID = SetHook(_proc);
+            Loaded += (s, e) => SetSystemCursorTo(HandCur);
             Closed += (s, e) =>
             {
                 RestoreDefaultCursors();
+                UnhookWindowsHookEx(_hookID);
             };
-#endif
+            // Initialize Kinect and program
             program = new Program(this);
         }
 
-        private static void SetSystemCursorToHand()
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var hHand = LoadCursor(IntPtr.Zero, (int)OCR_HAND);
-            if (hHand != IntPtr.Zero)
-            {
-                SetSystemCursor(hHand, OCR_NORMAL); // Set the system cursor to hand
-            }
+            throw new NotImplementedException();
         }
 
-        private static void RestoreDefaultCursors()
-        {
-            SystemParametersInfo(SPI_SETCURSORS, 0, IntPtr.Zero, 0);
-        }
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SystemParametersInfo(int uAction, uint uParam, IntPtr lpvParam, int fuWinIni);
+        [DllImport("user32.dll")]
+        private static extern bool SetSystemCursor(IntPtr hCursor, uint id);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr LoadCursorFromFile(string lpFileName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
